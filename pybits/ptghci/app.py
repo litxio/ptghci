litxio/ptghci

commit 957fc8102f9598709ef7097d66ddde0d103deffb

Fix tests broken by change to history/past

pybits/ptghci/app.py
@@ -22,8 +22,14 @@ def int_handler(sig, frame):
 
 class App:
     def __init__(self, PtPromptSession=PromptSession,
+                 history=None,
                  pt_print=print_formatted_text,
                  oop_engine=False):
+        if history is None:
+            self.history = ThreadedHistory(FileHistory(
+                os.path.expanduser(config.history_path)))
+        else:
+            self.history = history
         self.PtPromptSession = PtPromptSession
         self.pt_print = pt_print
         self.oop_engine = oop_engine
@@ -43,7 +49,7 @@ class App:
                 self.ngin = engine.Engine(self.config, pt_print=self.pt_print)
 
             self.session = session.Session(self.config, self.ngin,
-                                           self.PtPromptSession,
+                                           self.PtPromptSession, self.history,
                                            self.pt_print)
 
             self.dispatcher = dispatch.Dispatcher(self.session, self.config,

pybits/ptghci/session.py
@@ -25,15 +25,13 @@ def default_continuation(prompt_width, line_number, wrap_count):
 
 
 class Session():
-    def __init__(self, config, engine, PtPromptSession,
+    def __init__(self, config, engine, PtPromptSession, history,
                  pt_print=print_formatted_text):
         self.config = config
         self.engine = engine
         self.pt_print = pt_print
         bindings = self.setup_key_bindings()
         lexer = PygmentsLexer(PtgHaskellLexer)
-        history = ThreadedHistory(FileHistory(
-            os.path.expanduser(config.history_path)))
         self.initial_lines = len(list(history.load_history_strings()))
         pg_style = style_from_pygments_cls(get_style_by_name(config.style))
         bottom_toolbar_style = Style.from_dict({
@@ -142,7 +140,7 @@ class Session():
         session_start = (len(self.history.get_strings())
                          - self.get_cur_lineno())
         if hist_idx > session_start:
-            return str(hist_idx - session-start)
+            return str(hist_idx - session_start)
         else:
             return "h%d" % hist_idx
 

pybits/ptghci/magic/rerun.py
@@ -40,7 +40,7 @@ def handle_rerun(command, args, session, config, dispatcher):
 
     elif any([i >= len(hist_strings) for i in idxs]):
         resp = Response.from_error_message(
-                'Syntax error: %d outside range of past history' %
+                'Syntax error: %s outside range of past history' %
                 session.format_hist_idx(max(idxs)))
 
     else:

pybits/test/mock.py
@@ -58,6 +58,9 @@ class MockHistory:
     def get_strings(self):
         return self.hist_lines
 
+    def load_history_strings(self):
+        return self.hist_lines
+
 
 class PromptDriver(threading.Thread):
     def __init__(self):
@@ -84,17 +87,18 @@ class PromptDriver(threading.Thread):
 
     def run(self):
         outer = self
+        history = MockHistory()
         class MockPrompt:
             def __init__(self, *args, **kwargs):
                 self.args = args
                 self.kwargs = kwargs
-                self.history = MockHistory()
+                self.history = history
 
             def prompt(self):
                 v = outer.next_input()
                 self.history.add_line(v)
                 return v
-        self.app = app.App(MockPrompt, self.ptprint, oop_engine=True)
+        self.app = app.App(MockPrompt, history, self.ptprint, oop_engine=True)
         self.app.run()
 
     def expect(self, regex, timeout=None):

commit 49a739bcc754986a14ce38eda5b04bfb8bcf1f10

Fix python imports bug caused by 957fc8102f

pybits/ptghci/app.py
@@ -5,6 +5,7 @@ import traceback
 import colors
 import signal
 import threading
+from prompt_toolkit.history import FileHistory, ThreadedHistory
 from prompt_toolkit import print_formatted_text, ANSI
 from prompt_toolkit.shortcuts import PromptSession
 from ptghci import settings, engine, dispatch, \
@@ -25,9 +26,16 @@ class App:
                  history=None,
                  pt_print=print_formatted_text,
                  oop_engine=False):
+
+        if isinstance(threading.current_thread(), threading._MainThread):
+            signal.signal(signal.SIGINT, int_handler)
+
+        self.config = settings.Settings.load()
+        if self.config.settings_path:
+            print("Using settings file %s" % self.config.settings_path)
         if history is None:
             self.history = ThreadedHistory(FileHistory(
-                os.path.expanduser(config.history_path)))
+                os.path.expanduser(self.config.history_path)))
         else:
             self.history = history
         self.PtPromptSession = PtPromptSession
@@ -36,12 +44,6 @@ class App:
 
     def run(self):
 
-        if isinstance(threading.current_thread(), threading._MainThread):
-            signal.signal(signal.SIGINT, int_handler)
-
-        self.config = settings.Settings.load()
-        if self.config.settings_path:
-            print("Using settings file %s" % self.config.settings_path)
         try:
             if self.oop_engine:
                 self.ngin = engine.Engine.oop_engine(self.config, pt_print=self.pt_print)

pybits/ptghci/session.py
@@ -5,7 +5,6 @@ from .lexer import PtgHaskellLexer
 from pygments.styles import get_style_by_name
 from prompt_toolkit.application import get_app
 from prompt_toolkit import print_formatted_text
-from prompt_toolkit.history import FileHistory, ThreadedHistory
 from prompt_toolkit.filters import is_searching
 from prompt_toolkit.lexers import PygmentsLexer
 from prompt_toolkit.styles.pygments import style_from_pygments_cls