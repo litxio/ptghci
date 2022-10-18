6aa76f76b722df97212536f72dda5e1a24932bdc
Rekihyt <checkmateguy@gmail.com>
2022-10-17 19:21:49 -0700

    make compile with 9.0.2

--- src/Language/Haskell/PtGhci/Prelude.hs
+++ src/Language/Haskell/PtGhci/Prelude.hs
@@ -1,32 +1,35 @@
+module Language.Haskell.PtGhci.Prelude
+  ( module P,
+    module MicroLens,
+    module Language.Haskell.PtGhci.Prelude,
+    String,
+    StringConv,
+    toS,
+    -- , unwords
+  )
+where
 
-module Language.Haskell.PtGhci.Prelude 
-  ( module P
-  , module MicroLens
-  , module Language.Haskell.PtGhci.Prelude 
-  , String
-  -- , unwords
-) where 
-
-import Protolude as P hiding ((%), to, trace)
 -- import Prelude (unwords)
-import Lens.Micro as MicroLens
-import qualified Protolude as P
-import qualified Data.Text as T
-import Text.Printf
 import Control.Exception (throw)
+import qualified Data.Text as T
 import GHC.Base (String)
+import Lens.Micro as MicroLens
+import Protolude as P hiding (to, toS, trace, (%))
+import qualified Protolude as P
+import Protolude.Conv (StringConv, toS)
+import Text.Printf
 
 tail :: [a] -> Maybe [a]
 tail [] = Nothing
-tail (a:as) = Just as
+tail (a : as) = Just as
 
 (!?) :: [a] -> Int -> Maybe a
 (!?) [] _ = Nothing
-(!?) (x:ls) 0 = Just x
-(!?) (x:ls) i = ls !? (i-1)
+(!?) (x : ls) 0 = Just x
+(!?) (x : ls) i = ls !? (i - 1)
 
 -- tryP :: (Exception e) => Ptg a -> Ptg (Either e a)
 -- tryP (Ptg action) = do
 --   r <- ask
 --   runReaderT r action
---   return $ Ptg $ 
+--   return $ Ptg $
